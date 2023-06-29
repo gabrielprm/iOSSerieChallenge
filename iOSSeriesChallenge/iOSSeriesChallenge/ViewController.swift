2756baf655b90c473444834f3a423f2c9f5af9b0
Gabriel do Prado Moreira <gabrieldopradomoreira@MacBook-Pro-de-Gabriel.local>
2023-06-29 16:23:14 -0300

    Add: Creating the project

---
+++ iOSSeriesChallenge/iOSSeriesChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSSeriesChallenge
+//
+//  Created by Gabriel do Prado Moreira on 29/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
