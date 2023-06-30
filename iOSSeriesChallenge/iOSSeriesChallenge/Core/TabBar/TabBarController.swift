5574cd99460dc93636797514c11fbe6b00a20f8f
Gabriel do Prado Moreira <gabrieldopradomoreira@MacBook-Pro-de-Gabriel.local>
2023-06-29 23:16:36 -0300

    Setting up TabBar

---
+++ iOSSeriesChallenge/iOSSeriesChallenge/Core/TabBar/TabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarController.swift
+//  iOSSeriesChallenge
+//
+//  Created by Gabriel do Prado Moreira on 29/06/23.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTabBar()
+    }
+    
+    func setupTabBar() {
+        let movieListViewController = ViewController()
+        
+        let movieListNavigationController = setupNavigationController(image: UIImage(systemName: "popcorn.fill"), viewController: movieListViewController)
+        
+        viewControllers = [movieListNavigationController]
+    }
+    
+    func setupNavigationController(image: UIImage?, viewController: UIViewController) -> UINavigationController {
+        let navigationController = UINavigationController(rootViewController: viewController)
+        navigationController.tabBarItem.image = image
+        return navigationController
+    }
+}
